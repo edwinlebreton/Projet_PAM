ff723061892a1c07ee61aa6f48824d75e7956e49
led0764 <edwinlebreton64@gmail.com>
2019-12-23 16:27:59 +0100

    Récuperation contacts telephone vers ListView dans vue principale (utilisation d'adapter avec MyCustomAdapter, nouveau XML contact_info, nouvelle classe ContactsInfo)
    (Refactor var "lattitude" vers "latitude")

--- app/src/main/java/com/androidcourse/pam/projet/v1/MainActivity.java
+++ app/src/main/java/com/androidcourse/pam/projet/v1/MainActivity.java
@@ -1,32 +1,49 @@
 package com.androidcourse.pam.projet.v1;
 
+import android.annotation.TargetApi;
+import android.content.ContentResolver;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.location.Location;
+import android.os.Build;
+import android.provider.ContactsContract;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     private FusedLocationProviderClient mFusedLocationClient;
-    private LocationCallback mLocationCallback;
+    //private LocationCallback mLocationCallback;
     int MY_PERMISSION_ACCESS_COARSE_LOCATION = 0;
-    private String lattitude;
+    private String latitude;
     private String longitude;
+    public static final int PERMISSIONS_REQUEST_READ_CONTACTS = 1;
+    MyCustomAdapter dataAdapter = null;
+    ListView listView;
+    List<ContactsInfo> contactsInfoList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView = (ListView) findViewById(R.id.lstContacts);
+        listView.setAdapter(dataAdapter);
+        requestContactPermission();
     }
 
     public void confirmAppointment(View v){
@@ -35,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setCancelable(true);
         builder.setTitle("Confirmer RDV ?");
-        builder.setMessage("Avec "+nameOfChosenContact+" à lattitude:"+lattitude+" longitude:"+longitude);
+        builder.setMessage("Avec "+nameOfChosenContact+" à latitude:"+latitude+" longitude:"+longitude);
         builder.setPositiveButton("Confirm",
                 new DialogInterface.OnClickListener() {
                     @Override
@@ -58,8 +75,8 @@ public class MainActivity extends AppCompatActivity {
         // enregistrer rdv dans bdd
     }
 
-    public void getCoordinates(){
-        String numeroTest="0635213563";
+    public void getCoordinates() {
+        String numeroTest = "0635213563";
         String msgRDV = "Vous avez rendez-vous à: ";
 
         /****** Location Services ********/
@@ -77,12 +94,104 @@ public class MainActivity extends AppCompatActivity {
                 if (location == null) {
                     System.out.println("Location error");
                 } else {
-                    lattitude = Double.toString(location.getLatitude());
+                    latitude = Double.toString(location.getLatitude());
                     longitude = Double.toString(location.getLongitude());
                 }
             }
         });
-
-        //System.out.println(msgRDV+lattitude+longitude+" avec "+numeroTest);
     }
+
+        //System.out.println(msgRDV+latitude+longitude+" avec "+numeroTest);
+
+        public void requestContactPermission() {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                            android.Manifest.permission.READ_CONTACTS)) {
+                        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                        builder.setTitle("Read contacts access needed");
+                        builder.setPositiveButton(android.R.string.ok, null);
+                        builder.setMessage("Please enable access to contacts.");
+                        builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                            @TargetApi(Build.VERSION_CODES.M)
+                            @Override
+                            public void onDismiss(DialogInterface dialog) {
+                                requestPermissions(
+                                        new String[]
+                                                {android.Manifest.permission.READ_CONTACTS}
+                                        , PERMISSIONS_REQUEST_READ_CONTACTS);
+                            }
+                        });
+                        builder.show();
+                    } else {
+                        ActivityCompat.requestPermissions(this,
+                                new String[]{android.Manifest.permission.READ_CONTACTS},
+                                PERMISSIONS_REQUEST_READ_CONTACTS);
+                    }
+                } else {
+                    getContacts();
+                }
+            } else {
+                getContacts();
+            }
+        }
+
+        @Override
+        public void onRequestPermissionsResult(int requestCode,
+        String permissions[], int[] grantResults) {
+            switch (requestCode) {
+                case PERMISSIONS_REQUEST_READ_CONTACTS: {
+                    if (grantResults.length > 0
+                            && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                        getContacts();
+                    } else {
+                        Toast.makeText(this, "You have disabled a contacts permission", Toast.LENGTH_LONG).show();
+                    }
+                    return;
+                }
+            }
+        }
+
+        private void getContacts() {
+            ContentResolver contentResolver = getContentResolver();
+            String contactId = null;
+            String displayName = null;
+            contactsInfoList = new ArrayList<ContactsInfo>();
+            Cursor cursor = getContentResolver().query(ContactsContract.Contacts.CONTENT_URI, null, null, null, ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " ASC");
+            if (cursor.getCount() > 0) {
+                while (cursor.moveToNext()) {
+                    int hasPhoneNumber = Integer.parseInt(cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER)));
+                    if (hasPhoneNumber > 0) {
+
+                        ContactsInfo contactsInfo = new ContactsInfo();
+                        contactId = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+                        displayName = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+
+                        contactsInfo.setContactId(contactId);
+                        contactsInfo.setDisplayName(displayName);
+
+                        Cursor phoneCursor = getContentResolver().query(
+                                ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                                null,
+                                ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
+                                new String[]{contactId},
+                                null);
+
+                        if (phoneCursor.moveToNext()) {
+                            String phoneNumber = phoneCursor.getString(phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+
+                            contactsInfo.setPhoneNumber(phoneNumber);
+                        }
+
+                        phoneCursor.close();
+
+                        contactsInfoList.add(contactsInfo);
+                    }
+                }
+            }
+            cursor.close();
+
+            dataAdapter = new MyCustomAdapter(MainActivity.this, R.layout.contact_info, contactsInfoList);
+            listView.setAdapter(dataAdapter);
+        }
 }
